13e1c4d0ada62fd5afff2ccc3169dd29261859ed
Jon Wretlind <47186515+jonwretlind@users.noreply.github.com>
2022-01-22 07:41:33 -0700

    Delete snippets directory

--- snippets/tooltip.js
+++
@@ -1,8 +0,0 @@
-import './tooltip.css';
-import React, { Component } from 'react';
-
-export default function ToolTip() {
-    return (
-      <span className="help"><a href="" className="link">?</a></span>
-    )
-  }
