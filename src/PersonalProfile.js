9e39c7c27bd636904ab98a401cb089a8ecec776d
jonwretlind <47186515+jonwretlind@users.noreply.github.com>
2022-06-25 11:20:24 -0700

    error handling on fetch()

--- src/PersonalProfile.js
+++ src/PersonalProfile.js
@@ -24,40 +24,43 @@ class PersonalProfile extends Component {
     };
   }
 
- componentDidMount() {
-    fetch(api+userId)
+  componentDidMount() {
+    fetch(api + userId)
       .then(result => {
         return result.json();
-    }).then(data => {
-      const traverse = new Traverse();
-      let user = () => {
-        return (
-          <span>{data.firstname} {data.lastname}</span>
-        )
-      }
+      }).then(data => {
+        const traverse = new Traverse();
+        let user = () => {
+          return (
+            <span>{data.firstname} {data.lastname}</span>
+          )
+        }
 
-      let picture = () => {
-            var url = "/assets/" + data.picture;
-            return (
-              <img src={url} className="profile-pic" />
-            )
-      }
+        let picture = () => {
+          var url = "/assets/" + data.picture;
+          return (
+            <img src={url} className="profile-pic" />
+          )
+        }
 
-      this.setState({
-              Name: user(),
-              Picture: picture(),
-              Age: data.age,
-              Relationships: data.relationships,
-              Spouse: data.spousename,
-              Dependents: data.dependents,
-              Children: traverse.run(data.children),
-              Employer: data.employer
-            })
-    })
+        this.setState({
+          Name: user(),
+          Picture: picture(),
+          Age: data.age,
+          Relationships: data.relationships,
+          Spouse: data.spousename,
+          Dependents: data.dependents,
+          Children: traverse.run(data.children),
+          Employer: data.employer
+        })
+      }).catch((error) => {
+        // Handle the error
+        console.log(error);
+      })
   }
 
-record(key, val) {
-  //console.log(key + " --> " + val)
+  record(key, val) {
+    //console.log(key + " --> " + val)
     return (
       <div key={key} className="detail">
         <div className="label col1">
@@ -71,37 +74,37 @@ record(key, val) {
   }
 
 
-render() {
-  var records = [];
-  var len = Object.keys(this.state).length;
-  // loop through records
-  for (var i = 2; i <= len-2; i++) {
+  render() {
+    var records = [];
+    var len = Object.keys(this.state).length;
+    // loop through records
+    for (var i = 2; i <= len - 2; i++) {
       var keys = Object.keys(this.state);
       var vals = Object.values(this.state);
       // push to array of records
       records.push(this.record(keys[i], vals[i]));
     }
     //console.log(records);
-      return (
-        <Box sx={{
-          flexGrow: 1,
-          bgcolor: 'background.paper',
-          display: 'flex',
-          flexDirection: 'column',
-          component: 'div'
-        }} id="Profile">
-          <div className="profile-pic">
-            {this.state.Picture}
-          </div>
-          <div className="heading h3">
-            {this.state.Name}
-          </div>
-          <div className="records">
-            {records}
-          </div>
-       </Box>
-     )
-   }
+    return (
+      <Box sx={{
+        flexGrow: 1,
+        bgcolor: 'background.paper',
+        display: 'flex',
+        flexDirection: 'column',
+        component: 'div'
+      }} id="Profile">
+        <div className="profile-pic">
+          {this.state.Picture}
+        </div>
+        <div className="heading h3">
+          {this.state.Name}
+        </div>
+        <div className="records">
+          {records}
+        </div>
+      </Box>
+    )
+  }
 }
 
 export default PersonalProfile;
